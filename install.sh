ea58ee9da44e5ffbc4130bac10babf9b7f64c2e2
Lennart Nicolas Krebs <contact@lennartnicolas.de>
2020-09-12 12:25:08 +0200

    Install.sh change test

--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
 # Symlinking the dotfiles directory with stow(!)
+# Running brew.sh to make sure stow gets installed
+
+./brew.sh
 
 # make sure we have pulled in and updated any submodules
 git submodule init
 git submodule update
+
+#TEST COMMIT
