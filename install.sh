53d0f2f78f4281f48ee910fec3f4ae9bcf00afa7
Lennart Nicolas Krebs <contact@lennartnicolas.de>
2020-09-12 20:57:38 +0200

    New folder structure for Stow

--- install.sh
+++ install.sh
@@ -3,10 +3,44 @@
 # Symlinking the dotfiles directory with stow(!)
 # Running brew.sh to make sure stow gets installed
 
-./brew.sh
+# Uncomment if stow isn't installed yet 
+# ./brew.sh
 
 # make sure we have pulled in and updated any submodules
 git submodule init
 git submodule update
 
-#TEST COMMIT
+# run stow command for passed in directory($2) in location($1)
+
+base=(
+    bash
+)
+
+useronly=(
+    git # .extra not in the repository - contains gitconfig and commiter name
+)
+    
+
+stowit() {
+    usr=$1
+    app=$2
+    stow -v -R -t ${usr} ${app}
+}
+
+echo ""
+echo "Stowing apps for user: ${whoami}"
+
+# install apps available to local users and root
+for app in ${base[@]}; do
+    stowit "${HOME}" $app
+done
+
+# install only user space folders
+for app in ${useronly[@]}; do
+    if [[ "$(whoami)" = *"root"*]]; then
+        stowit "${HOME}" $app
+    fi
+done
+
+echo ""
+echo "#### ALL DONE"
